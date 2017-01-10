de30028a3e6718546ed22f7a176cb29d22c1e9ac
Shunji Makino <shunji.makino@gmail.com>
2017-01-11 00:51:36 +0900

    add hotentry feature

--- services/hatena_service.py
+++ services/hatena_service.py
@@ -1,17 +1,18 @@
 import urllib.request, urllib.parse, urllib.error
 import json
-
+import xmltodict
 
 class HatenaService:
 
-    _END_POINT = "http://b.hatena.ne.jp/entry/jsonlite/"
+    _ENTRY_END_POINT = 'http://b.hatena.ne.jp/entry/jsonlite/'
+    _HOT_ENTRY_END_POINT = 'http://b.hatena.ne.jp/hotentry'
 
     @classmethod
     def get_hatena_entry_by_url(cls, url):
         params = urllib.parse.urlencode({
             "url": url
         })
-        request_url = "{}?{}".format(cls._END_POINT, params)
+        request_url = "{}?{}".format(cls._ENTRY_END_POINT, params)
         result = urllib.request.urlopen(request_url).read()
         data = json.loads(result.decode('utf-8'))
         return data
@@ -23,3 +24,14 @@ class HatenaService:
             map(lambda bm: bm['comment'], entry['bookmarks'])
         ))
         return comments
+
+    @classmethod
+    def get_hotentry(cls, catecory=None):
+        if catecory:
+            request_url = "{}/{}.rss".format(cls._HOT_ENTRY_END_POINT, catecory)
+        else:
+            request_url = "{}{}".format(cls._HOT_ENTRY_END_POINT, '?mode=rss')
+        result = urllib.request.urlopen(request_url).read()
+        data = xmltodict.parse(result.decode('utf-8'))
+        return data
+
